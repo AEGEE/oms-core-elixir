06c8cdc58490a5757db732d75380e8edd195ff80
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-10-22 14:11:10 +0300

    Changed oms.aegee.org to my.aegee.eu

--- config/config.exs
+++ config/config.exs
@@ -19,7 +19,7 @@ end
 config :omscore,
   ecto_repos: [Omscore.Repo],
   env: Mix.env,
-  url_prefix: System.get_env("BASE_URL") || "oms.aegee.org",
+  url_prefix: System.get_env("BASE_URL") || "my.aegee.eu",
   ttl_refresh: 60 * 60 * 24 * 7 * 2,  # 2 weeks
   ttl_access: 60 * 60,                # 1 hour
   ttl_password_reset: 60 * 15,        # 15 Minutes
