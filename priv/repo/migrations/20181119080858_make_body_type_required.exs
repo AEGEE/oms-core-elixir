f207652a7bb427db67ace22731fdd104d1130161
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2018-11-19 09:26:03 +0100

    Setting a default will not apply the default to all bodies, needs update statement

--- priv/repo/migrations/20181119080858_make_body_type_required.exs
+++ priv/repo/migrations/20181119080858_make_body_type_required.exs
@@ -2,6 +2,8 @@ defmodule Omscore.Repo.Migrations.MakeBodyTypeRequired do
   use Ecto.Migration
 
   def change do
+    execute "UPDATE bodies SET type=`antenna` WHERE type IS NULL;"
+
     alter table(:bodies) do
       modify :type, :string, default: "antenna", null: false
 
