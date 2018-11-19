c7aa74b50a32558e41e09f62db1601be0599edfd
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2018-11-19 09:32:34 +0100

    Fixed SQL syntax

--- priv/repo/migrations/20181119080858_make_body_type_required.exs
+++ priv/repo/migrations/20181119080858_make_body_type_required.exs
@@ -2,7 +2,7 @@ defmodule Omscore.Repo.Migrations.MakeBodyTypeRequired do
   use Ecto.Migration
 
   def change do
-    execute "UPDATE bodies SET type=`antenna` WHERE type IS NULL;"
+    execute "UPDATE bodies SET type='antenna' WHERE type IS NULL;"
 
     alter table(:bodies) do
       modify :type, :string, default: "antenna", null: false
